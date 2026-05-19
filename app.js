220ab9cc9abf337c525b05c0ee8f1bbbfffd9da4
a14477-oficina <a14477@oficina.pt>
2026-05-19 11:52:57 +0000

    inicial commit

---
+++ app.js
@@ -0,0 +1,266 @@
+// Importa os módulos necessários
+const express = require('express');
+const mysql = require('mysql2/promise');
+
+// Configuração do servidor e banco de dados
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+
+
+
+// Conexão com o banco de dados
+// ter em conta o docker se estiver a utilizar as imagens / containers
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root', // Altere conforme necessário
+    password: 'root', // Altere conforme necessário
+    database: 'ipo'
+});
+
+
+
+
+
+
+
+
+
+
+// Rota para verificar se está ok !
+app.post('/', async (req, res) => {
+    try {
+        res.json({ msg: 'OK'});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+
+
+
+
+// Rota para obter todas as marcas
+app.get('/marcas', async (req, res) => {
+    try {
+        const [rows] = 
+        await db.query('SELECT * FROM marca ORDER BY marca asc;');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// Rota para obter todas os empregados
+app.get('/empregados', async (req, res) => {
+    try {
+        const [rows] = 
+        await db.query('SELECT * FROM empregados ORDER BY id asc;');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para obter todas as lojas
+app.get('/lojas', async (req, res) => {
+    try {
+        const [rows] = 
+        await db.query('SELECT * FROM lojas ORDER BY stamp asc;');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// Rota para obter uma MARCA por codmarca
+app.get('/marcas/:cod', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM marca WHERE codmarca = ?';
+        const [rows] = await db.query(sql, [req.params.cod]);
+        if (rows.length === 0) {
+            return res.status(404).
+                    json({ error: 'Marca não encontrada' });
+        }
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para obter uma loja por id
+app.get('/lojas/:id', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM lojas WHERE stamp = ?';
+        const [rows] = await db.query(sql, [req.params.cod]);
+        if (rows.length === 0) {
+            return res.status(404).
+                    json({ error: 'loja não encontrada' });
+        }
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para obter os empregados por id
+app.get('/empregados/:id', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM empregados WHERE id = ?';
+        const [rows] = await db.query(sql, [req.params.cod]);
+        if (rows.length === 0) {
+            return res.status(404).
+                    json({ error: 'Marca não encontrada' });
+        }
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+// Rota para criar uma nova marca
+app.post('/marcas/inserir', async (req, res) => {
+    try {
+        const { marca } = req.body;
+        const sql = 'INSERT INTO marca (marca) VALUES (?)';
+        const [result] = await db.query(sql, [marca]);
+        res.json({ id: result.insertId, marca });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para inserir uma nova loja
+app.post('/lojas/inserir', async (req, res) => {
+    try {
+        const { nome, local, telefone, email, website } = req.body;
+        const sql = 'INSERT INTO lojas (nome, local, telefone, email, website) VALUES (?,?,?,?,?)';
+        const [result] = await db.query(sql, [nome, local, telefone, email, website]);
+        res.json({ id: result.insertId, nome, local, telefone, email, website });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para inserir um novo empregado
+app.post('/empregados/inserir', async (req, res) => {
+    try {
+        const { id, idloja, nome, funcao, email } = req.body;
+        const sql = 'INSERT INTO lojas (id, idloja, nome, funcao, email) VALUES (?,?,?,?,?)';
+        const [result] = await db.query(sql, [id, idloja, nome, funcao, email]);
+        res.json({ id: result.insertId, id, idloja, nome, funcao, email });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para eliminar marca
+app.post('/marca/eliminar/:cod', async (req, res) => {
+    try {
+        const sql  = 'DELETE FROM marca WHERE codmarca = ?';
+        const [result] = await db.query(sql, [req.params.cod]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'Marca não encontrada' });
+        }
+        res.json({ message: 'Marca eliminada com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para eliminar uma loja
+app.post('/lojas/eliminar/:id', async (req, res) => {
+    try {
+        const sql  = 'DELETE FROM lojas WHERE stamp = ?';
+        const [result] = await db.query(sql, [req.params.stamp]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'loja não encontrada' });
+        }
+        res.json({ message: 'loja eliminada com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para eliminar um empregado
+app.post('/empregados/eliminar/:id', async (req, res) => {
+    try {
+        const sql  = 'DELETE FROM empregados WHERE id = ?';
+        const [result] = await db.query(sql, [req.params.stamp]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'empregado não encontrada' });
+        }
+        res.json({ message: 'empregado eliminada com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+
+
+
+
+
+// Rota para obter X:limit clientes a partir de Y:ofsset regitos ordenados por nome aSC
+app.get('/clientes/:offset/:limit', async (req, res) => {
+    try {
+        const [rows] = 
+        await db.query('SELECT * FROM cliente ORDER BY nome asc LIMIT ?,?;', 
+                [Number(req.params.offset), Number(req.params.limit)]);
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para criar um novo cliente
+app.post('/cliente/inserir', async (req, res) => {
+    try {
+        const { nome, morada, nif } = req.body;
+        const sql = 'INSERT INTO cliente (nome, morada, nif) VALUES (?, ?, ?)';
+        const [result] = await db.query(sql, [nome, morada, nif]);
+        res.json({ id: result.insertId, nome, morada, nif });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para eliminar cliente
+app.post('/cliente/eliminar/:cod', async (req, res) => {
+    try {
+        const sql  = 'DELETE FROM cliente WHERE codcli = ?';
+        const [result] = await db.query(sql, [req.params.cod]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'Cliente não encontrado' });
+        }
+        res.json({ message: 'Cliente eliminado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+// node app.js
+
+
+
